5972eb7e2bd44b43dda4d869aca28ea4a0a6cc0c
kgrubb <madison.grubb@sailr.co>
2019-10-03 00:40:07 -0400

    ignore build dir output

--- workflows.sh
+++ workflows.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 export DEBIAN_FRONTEND=noninteractive
 
-echo: "pwd: $(pwd)"
-ls
-
 CHANNEL="$1"
 VERSION="$2"
 
@@ -18,22 +15,22 @@ apt-get -y install software-properties-common
 add-apt-repository -u -y ppa:elementary-os/os-patches
 add-apt-repository -u -y ppa:elementary-os/"$CHANNEL"
 apt-get install -y elementary-os-overlay
-apt-get update
-apt-get -y dist-upgrade
+apt-get -qq update
+apt-get -q -y dist-upgrade
 bash -c 'echo "deb http://packages.elementary.io/appcenter $(lsb_release -sc) main" >> /etc/apt/sources.list.d/appcenter.list'
 wget -q --show-progress --progress=bar:force:noscroll  2>&1 -O /etc/apt/trusted.gpg.d/appcenter.asc http://packages.elementary.io/key.asc
-apt-get install --no-install-recommends -y elementary-sdk
-apt-get -q -y install git devscripts debhelper dctrl-tools dpkg-dev genisoimage gfxboot-theme-ubuntu isolinux live-build squashfs-tools syslinux syslinux-utils
+apt-get -q -y install --no-install-recommends elementary-sdk
+apt-get -q -y install sudo git devscripts debhelper dctrl-tools dpkg-dev genisoimage gfxboot-theme-ubuntu isolinux live-build squashfs-tools syslinux syslinux-utils
 
-git clone --depth=1 https://github.com/elementary/syslinux-theme.git && cd syslinux-theme
-debuild -S -sd
+# git clone --depth=1 https://github.com/elementary/syslinux-theme.git && cd syslinux-theme
+# debuild -S -sd
 
-sed -i "s/CHANNEL=\"stable\"/CHANNEL=\"$CHANNEL\"/" etc/terraform.conf
-sed -i "s/VERSION=\"5.0\"/VERSION=\"$VERSION\"/" etc/terraform.conf
+sed -i "s/CHANNEL=\"stable\"/CHANNEL=\"$CHANNEL\"/" ./etc/terraform.conf
+sed -i "s/VERSION=\"5.0\"/VERSION=\"$VERSION\"/" ./etc/terraform.conf
 
 echo -e "
 #----------------------#
 # RUN TERRAFORM SCRIPT #
 #----------------------#
 "
-sudo ./terraform.sh
+sudo bash ./terraform.sh
