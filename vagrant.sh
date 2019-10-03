5c32f6116afe5fc328d3ec4afeeae6f6d4a97363
kgrubb <madison.grubb@sailr.co>
2019-10-03 13:48:17 -0400

    try manually building syslinux theme for now

--- vagrant.sh
+++ vagrant.sh
@@ -28,6 +28,12 @@ debuild -S -sd
 
 sed -i "s/CHANNEL=\"stable\"/CHANNEL=\"daily\"/" etc/terraform.conf
 
+# manually build this for now, until it's builds are automated.
+git clone --depth=1 https://github.com/elementary/syslinux-theme.git && (cd syslinux-theme || exit 1)
+debuild -us -uc -b
+dpkg-deb -b debian/syslinux-themes-elementary-juno
+dpkg -i debian/syslinux-themes-elementary-juno.deb
+
 echo -e "
 #----------------------#
 # RUN TERRAFORM SCRIPT #
--- workflows.sh
+++ workflows.sh
@@ -10,8 +10,6 @@ echo -e "
 #----------------------#
 "
 
-apt-get -qq update
-apt-get -y install software-properties-common
 add-apt-repository -u -y ppa:elementary-os/os-patches
 add-apt-repository -u -y ppa:elementary-os/"$CHANNEL"
 apt-get install -y elementary-os-overlay
@@ -19,11 +17,27 @@ apt-get -qq update
 apt-get -q -y dist-upgrade
 bash -c 'echo "deb http://packages.elementary.io/appcenter $(lsb_release -sc) main" >> /etc/apt/sources.list.d/appcenter.list'
 wget -q --show-progress --progress=bar:force:noscroll  2>&1 -O /etc/apt/trusted.gpg.d/appcenter.asc http://packages.elementary.io/key.asc
-apt-get -q -y install --no-install-recommends elementary-sdk
-apt-get -q -y install sudo git devscripts debhelper dctrl-tools dpkg-dev genisoimage gfxboot-theme-ubuntu isolinux live-build squashfs-tools syslinux syslinux-utils
+apt-get -q -y install --no-install-recommends \
+    elementary-sdk \
+    sudo \
+    git \
+    devscripts \
+    debhelper \
+    dctrl-tools \
+    dpkg-dev \
+    genisoimage \
+    gfxboot-theme-ubuntu \
+    isolinux \
+    live-build \
+    squashfs-tools \
+    syslinux \
+    syslinux-utils
 
-# git clone --depth=1 https://github.com/elementary/syslinux-theme.git && cd syslinux-theme
-# debuild -S -sd
+# manually build this for now, until it's builds are automated.
+git clone --depth=1 https://github.com/elementary/syslinux-theme.git && (cd syslinux-theme || exit 1)
+debuild -us -uc -b
+dpkg-deb -b debian/syslinux-themes-elementary-juno
+dpkg -i debian/syslinux-themes-elementary-juno.deb
 
 sed -i "s/CHANNEL=\"stable\"/CHANNEL=\"$CHANNEL\"/" ./etc/terraform.conf
 sed -i "s/VERSION=\"5.0\"/VERSION=\"$VERSION\"/" ./etc/terraform.conf
