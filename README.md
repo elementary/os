e7a6fcf9a66f971d274c3fdaef8b56ab8f609290
Keli Grubb <keligrubb324@gmail.com>
2019-10-09 22:06:05 -0400

    Add build system (#231)
    * add build system
    
    * fix vagrant build to default to daily instead of stable builds
    
    * allow version tagging and channel definitions in workflows
    
    * fix invalid syntax
    
    * Try fixing stable workflow
    
    * Update daily builder for testing
    
    * mark workflows.sh as executable
    
    * add sudo to github workflow build task
    
    * update README
    
    * get info on working directory and directory contents in pipeline
    
    * ignore build dir output
    
    * try manually building syslinux theme for now
    
    * fix invalid cd opt
    
    * fix path issue, add syslinux-common package requirement
    
    * try to upload after build completes
    
    * use boto for file uploads
    
    * set secrets in yml config
    
    * fix typo :speak_no_evil:
    
    * update daily workflow to run on branches with 'build' prefix. Update stable to include upload endpoints
    
    * use channel for upload path. rename shasum and md5 filename with build date to make visual grepping easier.
    
    * set acl for objects. run another build to test new folder/path policies
    
    * use boto3 acl setup instead of boto2
    
    * include extra arg in upload to set public read access
    
    * Remove un-needed workaround hooks. Use git instead of bzr for seed fetching. Link initrd.lz to initrd to try and fix EFI booting. Remove unused desltop list packages. Upgrade to hwe packages.
    
    * point to initrd.lz instead of initrd in grub config
    
    * trigger another build
    
    * rename steps for badges to work. make daily builds run on merge to master

--- README.md
+++ README.md
@@ -1,3 +1,50 @@
-# os
+<div align="center">
+  <a href="https://elementary.io" align="center">
+    <center align="center">
+      <img src="https://raw.githubusercontent.com/elementary/brand/master/logomark-black.png" alt="elementary" align="center">
+    </center>
+  </a>
+  <br>
+  <h1 align="center"><center>elementary OS</center></h1>
+  <h3 align="center"><center>Build scripts for image creation</center></h3>
+  <br>
+  <br>
+</div>
 
-Dummy repo to track packaging and configuration issues. If in doubt, report issues here and we can transfer them to the correct repo.
+<p align="center">
+  <img src="https://github.com/elementary/os/workflows/stable/badge.svg" alt="Stable">
+  <img src="https://github.com/elementary/os/workflows/daily/badge.svg" alt="Daily">
+</p>
+
+---
+
+## Building Locally
+
+To build an elementary OS image you'll need to:
+
+ 1) Make sure you have the following dependencies installed:
+
+    * `dctrl-tools`
+    * `dpkg-dev`
+    * `genisoimage`
+    * `gfxboot-theme-ubuntu`
+    * `live-build`
+    * `squashfs-tools`
+    * `syslinux`
+    * `zsync`
+
+ 2) Clone this project & `cd` into it:
+
+    ```
+    git clone https://github.com/elementary/os && cd os
+    ```
+
+ 3) Configure the channel in the `etc/terraform.conf` (stable, daily).
+
+ 4) Run the build script as a root user:
+
+    ```
+    ./terraform.sh
+    ```
+
+ 5) When done, your images will be in the builds folder.
---
+++ vagrant.sh
@@ -0,0 +1,54 @@
+#!/bin/bash
+export DEBIAN_FRONTEND=noninteractive
+
+# copy project into home dir to avoid double-mount issues withe the /vagrant dir
+cp -r /vagrant/* ~/
+cd ~ || exit 1
+
+echo -e "
+#----------------------#
+# INSTALL DEPENDENCIES #
+#----------------------#
+"
+
+apt-get -y install software-properties-common
+add-apt-repository -u -y ppa:elementary-os/os-patches
+add-apt-repository -u -y ppa:elementary-os/daily
+apt-get install -y elementary-os-overlay
+apt-get update
+apt-get -y dist-upgrade
+bash -c 'echo "deb http://packages.elementary.io/appcenter $(lsb_release -sc) main" >> /etc/apt/sources.list.d/appcenter.list'
+wget -q --show-progress --progress=bar:force:noscroll  2>&1 -O /etc/apt/trusted.gpg.d/appcenter.asc http://packages.elementary.io/key.asc
+apt-get install --no-install-recommends -y elementary-sdk
+apt-get -q -y install --no-install-recommends \
+    elementary-sdk \
+    sudo \
+    git \
+    devscripts \
+    debhelper \
+    dctrl-tools \
+    dpkg-dev \
+    genisoimage \
+    gfxboot-theme-ubuntu \
+    isolinux \
+    live-build \
+    squashfs-tools \
+    syslinux \
+    syslinux-utils \
+    syslinux-common
+
+# manually build this for now, until it's builds are automated.
+git clone --depth=1 https://github.com/elementary/syslinux-theme.git && cd syslinux-theme || exit 1
+debuild -us -uc -b
+dpkg-deb -b debian/syslinux-themes-elementary-juno
+dpkg -i debian/syslinux-themes-elementary-juno.deb
+cd .. || exit 1
+
+sed -i "s/CHANNEL=\"stable\"/CHANNEL=\"daily\"/" etc/terraform.conf
+
+echo -e "
+#----------------------#
+# RUN TERRAFORM SCRIPT #
+#----------------------#
+"
+./terraform.sh
