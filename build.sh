eb40e4bbbfe582b8eef6dccb0af33b6648b01b93
David Hewitt <davidmhewitt@gmail.com>
2019-10-23 10:57:47 +0100

    Install ubuntu-keyring in Debian host

--- build.sh
+++ build.sh
@@ -11,7 +11,7 @@ echo -e "
 "
 
 apt-get update
-apt-get install -y live-build patch
+apt-get install -y live-build patch ubuntu-keyring
 
 patch -d /usr/lib/live/build/ < live-build-fix-syslinux.patch
 
