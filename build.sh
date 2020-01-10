d6bd668dce09f5ecf1f2ee3adecc051881609e7c
kgrubb <keligrubb324@gmail.com>
2020-01-10 18:03:01 -0500

    symlink focal build script to gutsy until debootstrap 1.0.117 or newer is released.

--- build.sh
+++ build.sh
@@ -15,6 +15,10 @@ apt-get install -y live-build patch ubuntu-keyring
 
 patch -d /usr/lib/live/build/ < live-build-fix-syslinux.patch
 
+# TODO: Remove this once debootstrap 1.0.117 or newer is released and available:
+# https://salsa.debian.org/installer-team/debootstrap/blob/master/debian/changelog
+ln -sfn /usr/share/debootstrap/scripts/gutsy /usr/share/debootstrap/scripts/focal
+
 echo -e "
 #----------------------#
 # RUN TERRAFORM SCRIPT #
