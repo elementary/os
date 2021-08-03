d9cacbaaafe9515cbf5bcfeac041af02b6dc8466
Daniel Foré <daniel@elementary.io>
2021-08-03 16:01:52 -0700

    Build: Remove focal/gutsy symlink

--- build.sh
+++ build.sh
@@ -30,10 +30,6 @@ apt-get install -y live-build patch ubuntu-keyring
 # https://salsa.debian.org/live-team/live-build/merge_requests/31
 patch -d /usr/lib/live/build/ < live-build-fix-syslinux.patch
 
-# TODO: Remove this once debootstrap 1.0.117 or newer is released and available:
-# https://salsa.debian.org/installer-team/debootstrap/blob/master/debian/changelog
-ln -sfn /usr/share/debootstrap/scripts/gutsy /usr/share/debootstrap/scripts/focal
-
 build () {
   BUILD_ARCH="$1"
 
