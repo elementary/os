60e6de2a66812ce7b3f9ce0cf2d02c66ade584e7
stsdc <6031763+stsdc@users.noreply.github.com>
2024-11-04 10:57:38 +0100

    Update trusted-firmware-a url

--- build-pinebookpro.sh
+++ build-pinebookpro.sh
@@ -30,26 +30,26 @@ export DEBIAN_FRONTEND="noninteractive"
 apt-get update
 apt-get install -y --no-install-recommends python3 bzip2 wget gcc-arm-none-eabi crossbuild-essential-arm64 make bison flex bc device-tree-compiler ca-certificates sed build-essential debootstrap qemu-user-static qemu-utils qemu-system-arm binfmt-support parted kpartx rsync git libssl-dev xz-utils coreutils util-linux
 
-wget "https://git.trustedfirmware.org/TF-A/trusted-firmware-a.git/snapshot/trusted-firmware-a-$tfaver.tar.gz"
+wget -O arm-trusted-firmware-${tfaver}.tar.gz https://github.com/ARM-software/arm-trusted-firmware/archive/refs/tags/v$tfaver.tar.gz
 wget "ftp://ftp.denx.de/pub/u-boot/u-boot-${ubootver}.tar.bz2"
 
-echo "37f917922bcef181164908c470a2f941006791c0113d738c498d39d95d543b21 trusted-firmware-a-${tfaver}.tar.gz" | sha256sum --check
+echo "304d372327d6ecabf89da67e2e1a7b2660f13b5851886fef1b58ae5a6d74e606 arm-trusted-firmware-${tfaver}.tar.gz" | sha256sum --check
 echo "c1f5bf9ee6bb6e648edbf19ce2ca9452f614b08a9f886f1a566aa42e8cf05f6a u-boot-${ubootver}.tar.bz2" | sha256sum --check
 
-tar xf "trusted-firmware-a-${tfaver}.tar.gz"
+tar xf "arm-trusted-firmware-${tfaver}.tar.gz"
 tar xf "u-boot-${ubootver}.tar.bz2"
 
 # These tarballs are extracted now, delete them
-rm "trusted-firmware-a-${tfaver}.tar.gz"
+rm "arm-trusted-firmware-${tfaver}.tar.gz"
 rm "u-boot-${ubootver}.tar.bz2"
 
-cd "trusted-firmware-a-${tfaver}"
+cd "arm-trusted-firmware-${tfaver}"
 unset CFLAGS CXXFLAGS CPPFLAGS LDFLAGS
 CROSS_COMPILE=aarch64-linux-gnu- make PLAT=rk3399
 cp build/rk3399/release/bl31/bl31.elf ../u-boot-${ubootver}/
 
 # We've got the compiled binary we need out of this folder, clean it up
-rm -rf "${basedir}/trusted-firmware-a-${tfaver}"
+rm -rf "${basedir}/arm-trusted-firmware-${tfaver}"
 
 cd ../u-boot-${ubootver}
 
